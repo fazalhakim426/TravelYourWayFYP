f7b58ddea298d8ab62c3c81b45d34aa3a6f4a63c
Fazal Hakim <hakimfazal426@gmail.com>
2021-06-14 23:38:02 +0500

    newcommit2

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -31,11 +31,13 @@ class Customer extends Model
     {
         //    return $this->visas()->count;
            $incomplete = $this->visas()->whereStatus('Incomplete')->get();
+           $cancel = $this->visas()->whereStatus('Cancel')->get();
            $completed = $this->visas()->whereStatus('Completed')->get();
            $payment_request = $this->visas()->whereStatus('payment_request')->get();
            $paid = $this->visas()->whereStatus('Paid')->get();
            $done = $this->visas()->whereStatus('Done')->get();
-        $status['visa']['incomplete']=count($incomplete);
+           $status['visa']['incomplete']=count($incomplete);
+           $status['visa']['cancel']=count($cancel);
         $status['visa']['completed']=count($completed);
         $status['visa']['payment_request']=count($payment_request);
         $status['visa']['paid']=count($paid);
@@ -44,12 +46,13 @@ class Customer extends Model
 
         
         $incomplete = $this->ticket()->whereStatus('Incomplete')->get();
+        $cancel = $this->ticket()->whereStatus('Cancel')->get();
         $completed = $this->ticket()->whereStatus('Completed')->get();
         $payment_request = $this->ticket()->whereStatus('payment_request')->get();
         $paid = $this->ticket()->whereStatus('Paid')->get();
         $done = $this->ticket()->whereStatus('Done')->get();
      $status['ticket']['incomplete']=count($incomplete);
-     $status['ticket']['completed']=count($completed);
+     $status['ticket']['cancel']=count($cancel);
      $status['ticket']['payment_request']=count($payment_request);
      $status['ticket']['paid']=count($paid);
      $status['ticket']['done']=count($done);
